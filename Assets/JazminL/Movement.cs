c91190b4e22c61acaaf376683d7454a2188a7b39
Jazmin <ijazmin98@hotmail.com>
2021-02-08 10:32:46 -0600

    Movimiento con teclas en camara

---
+++ Assets/JazminL/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float jumpforce;
+    public bool isGrounded;
+    public bool isSecondJumpAvailable;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += -transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0f, -1f, 0f) * rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0f, 1f, 0f) * rotationSpeed * Time.deltaTime);
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || isSecondJumpAvailable))
+        {
+            Rigidbody rigibody = GetComponent<Rigidbody>();
+            rigibody.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            transform.position += Vector3.up * speed * Time.deltaTime;
+
+            if (!isGrounded && isSecondJumpAvailable)
+            {
+                isSecondJumpAvailable = false;
+            }
+        }
+    }
+}
