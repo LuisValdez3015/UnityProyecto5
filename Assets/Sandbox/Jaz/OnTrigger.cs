01d742b6847705da089028aea829e8fa5941ed38
Jazmin <ijazmin98@hotmail.com>
2021-03-01 01:26:23 -0600

    Se agrego un on tigger enter y un set active, solo faltaria asignar que se abriria

---
+++ Assets/Sandbox/Jaz/OnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTrigger : MonoBehaviour
+{
+    public GameObject CuboAzul;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CuboAzul.SetActive(true);
+    }
+
+
+}
