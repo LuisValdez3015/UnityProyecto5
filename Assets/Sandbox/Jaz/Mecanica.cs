d0fb5ef38fc79aeebf4c3843722a1564571b0167
Jazmin <ijazmin98@hotmail.com>
2021-03-01 10:39:49 -0600

    Se agrego un on trigger enter y se abre una puerta

---
+++ Assets/Sandbox/Jaz/Mecanica.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mecanica : MonoBehaviour
+{
+    public GameObject CuboAzul;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CuboAzul.SetActive(true);
+    }
+
+
+
+}
---
+++ Assets/Sandbox/Jaz/MoveDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDoor : MonoBehaviour
+{
+    public GameObject door;
+    bool isDooropen;
+    public float speed;
+
+    public void OpenDoor()
+    {
+        door.transform.position += transform.up * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isDooropen)
+        {
+            OpenDoor();
+            isDooropen = true;
+            this.gameObject.SetActive(false);
+        }
+    }
+
+}
